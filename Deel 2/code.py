ebeff5a48786c48b90fc0c5b170704b6a7e58ef6
Gaby Hageman <gabyhageman2@gmail.com>
2020-01-07 14:28:52 +0100

    verbeterheuristieken

--- Deel 2/code.py
+++ Deel 2/code.py
@@ -6,18 +6,18 @@ class Routes():
         self.startend = []
         self.total = []
 
-        with open('Bijlage/ConnectiesHolland.csv', 'rt') as csv_file:
+        with open('Bijlage/ConnectiesNationaal.csv', 'rt') as csv_file:
             reader = csv.reader(csv_file, delimiter=',')
             for row in reader: 
                 if row[0] not in self.connections:
                     self.connections[row[0]] = {}
                     self.total.append(row[0])
-                self.connections[row[0]][row[1]] = row[2]
+                self.connections[row[0]][row[1]] = int(float(row[2]))
 
                 if row[1] not in self.connections:
                     self.connections[row[1]] = {}
                     self.total.append(row[1])
-                self.connections[row[1]][row[0]] = row[2]
+                self.connections[row[1]][row[0]] = int(float(row[2]))
 
             for key, value in self.connections.items():
                 if len(value) == 1:
@@ -34,14 +34,15 @@ class Routes():
         for city in self.start_end:
             self.maketraject(city, count, maxtime)
             count += 1
-
+            
         # Overige routes
         while len(self.stations) != 0:
             for city in self.stations:
                 self.maketraject(city, count, maxtime)
                 count += 1
-
+                
         return self.quality()
+
 
     def maketraject(self, city, count, maxtime):
         endtime = 0
@@ -50,7 +51,7 @@ class Routes():
         traject.append(city)
         self.stations.remove(city)
 
-        while time < maxtime and count <= 7:
+        while time < maxtime and count <= 20:
             best_stop_time = 100
             best_stop_city = ""
             for connection in self.connections[city]:
@@ -99,7 +100,7 @@ class Routes():
         solution_old = 0
         best_traject = {}
 
-        while maxtime <= 120:
+        while maxtime <= 180:
             solution = self.startsolution(maxtime)
             if solution > solution_old:
                 solution_old = solution
@@ -109,7 +110,6 @@ class Routes():
             maxtime += 1
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     routes = Routes()
     routes.heuristiek()
-        
