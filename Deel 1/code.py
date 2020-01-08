c0518daec6aa9e5e72bdb9fbce01c7ad1ffe4b07
Perry Plooij <perryplooy@gmail.com>
2020-01-08 13:45:36 +0100

    added test.py

--- Deel 1/code.py
+++ Deel 1/code.py
@@ -1,60 +1,65 @@
 import csv
+import matplotlib.pyplot as plt
 
 class Routes():
     def __init__(self):
         self.connections = {}
-        self.trajects = {}
-        self.start_end = []
-        self.stations = []
+        self.startend = []
+        self.total = []
 
         with open('Bijlage/ConnectiesHolland.csv', 'rt') as csv_file:
             reader = csv.reader(csv_file, delimiter=',')
             for row in reader: 
                 if row[0] not in self.connections:
                     self.connections[row[0]] = {}
-                    self.stations.append(row[0])
+                    self.total.append(row[0])
                 self.connections[row[0]][row[1]] = row[2]
 
                 if row[1] not in self.connections:
                     self.connections[row[1]] = {}
-                    self.stations.append(row[1])
+                    self.total.append(row[1])
                 self.connections[row[1]][row[0]] = row[2]
 
             for key, value in self.connections.items():
                 if len(value) == 1:
-                    self.start_end.append(key)
+                    self.startend.append(key)
 
-
-    def startsolution(self):
+    def startsolution(self, maxtime):
         count = 1
+        self.trajects = {}
+        self.stations = self.total.copy()
+        self.start_end = self.startend.copy()
 
         # Startroutes
         for city in self.start_end:
-            self.maketraject(city, count)
+            self.maketraject(city, count, maxtime)
             count += 1
 
         # Overige routes
         while len(self.stations) != 0:
             for city in self.stations:
-                self.maketraject(city, count)
+                self.maketraject(city, count, maxtime)
                 count += 1
 
-        self.quality()
+        return self.quality()
 
-    def maketraject(self, city, count):
+
+    def maketraject(self, city, count, maxtime):
         endtime = 0
         time = 0
         traject = []
+
         traject.append(city)
         self.stations.remove(city)
 
-        while time < 120:
+        while time < maxtime and count <= 7:
             best_stop_time = 100
             best_stop_city = ""
+
             for connection in self.connections[city]:
                 time_traject = int(self.connections[city][connection])
 
-                if time_traject < best_stop_time and connection in self.stations and time + time_traject <= 120:
+                if time_traject < best_stop_time and connection in self.stations and time + time_traject <= maxtime:
                     best_stop_time = time_traject
                     best_stop_city = connection
             
@@ -64,9 +69,12 @@ class Routes():
                 city = best_stop_city
                 traject.append(city)
                 self.stations.remove(city)
+
+                if city in self.start_end:
+                    self.start_end.remove(city)
             else:
                 endtime = time
-                time = 120
+                time = maxtime
 
             if len(traject) == 1:
                 connections = self.connections[traject[0]]
@@ -86,9 +94,32 @@ class Routes():
             minutes += value[1]
 
         K = p * 10000 - (T * 100 + minutes)
-        print(K)
+        return K
 
 
-if __name__ == "__main__": 
-    Routes()
-    Routes().startsolution()
+    def heuristiek(self):
+        maxtime = 0
+        solution_old = 0
+        best_traject = {}
+
+        while maxtime <= 120:
+            solution = self.startsolution(maxtime)
+
+            if solution > solution_old:
+                solution_old = solution
+                best_traject = self.trajects
+            maxtime += 1
+
+        # number = [1,2,3,4]
+        # number2 = [2,4,1,7]
+        # plt.plot(number,number2)
+        # plt.show()
+
+    def random(self):
+        pass
+        
+
+if __name__ == "__main__":
+    routes = Routes()
+    routes.heuristiek()
+        
