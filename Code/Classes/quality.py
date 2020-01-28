f9c5432e168ce640d5c23ef7636c9d0dec4b7024
Gaby Hageman <gabyhageman2@gmail.com>
2020-01-28 09:42:40 +0100

    comments

--- Code/Classes/quality.py
+++ Code/Classes/quality.py
@@ -1,30 +1,14 @@
-# ******************************************************************************************************
-# * quality.py
-# *
-# * PGT Party
-# *
-# * Calculate the quality of a train lining system based on the fraction connections used relative
-# * to the total amount of connections, the amount of trajects and the total time of the lining system. 
-# ******************************************************************************************************
+def calculate_quality(connections, connection_total, trajects):
+    """ Calculate quality of the created train lining system based on the fraction connections used, the amount of
+        trajects and the total time of the trian lining system.
+    """
 
+    minutes = 0
+    p = 1 - connections / connection_total
+    T = len(trajects)
 
-class Quality():
-    def __init__(self, connections, connection_total, trajects):
-        self.connections = connections
-        self.connection_total = connection_total
-        self.trajects = trajects
+    for key, value in trajects.items():
+        minutes += value[1]
 
-        self.quality = self.calculate_quality()
-
-    def calculate_quality(self):
-        """ Calculate quality of the created train lining system """
-
-        minutes = 0
-        p = 1 - self.connections / self.connection_total
-        T = len(self.trajects)
-
-        for key, value in self.trajects.items():
-            minutes += value[1]
-
-        K = p * 10000 - (T * 100 + minutes)
-        return K
+    K = p * 10000 - (T * 100 + minutes)
+    return K
--- Code/Classes/solution.py
+++ Code/Classes/solution.py
@@ -1,13 +1,3 @@
-# ******************************************************************************************************
-# * solution.py
-# *
-# * PGT Party
-# *
-# * 
-# ******************************************************************************************************
-
-
-
 import csv
 import time
 
@@ -16,51 +6,50 @@ from time import gmtime, strftime
 
 import matplotlib.pyplot as plt
 
-class Solution():
-    def __init__(self, traject, quality, stations):
-        self.traject = traject
-        self.quality = quality
-        self.stations = stations
 
-    def visualisation(self):
-        """ Make a visualisation of the best timetable """
+def visualisation(traject, all_stations):
+    """ 
+        Make a visualisation of the best train lining system based on the coordinates of the stations and the trajects. 
+    """
 
-        colors = ["green", "red", "aqua", "orange", "yellow", "lawngreen", "deepskyblue", "violet", "pink", "deeppink", "darkviolet", "grey", "salmon", "gold", "mediumseagreen", "mediumturquoise", "darkkhaki", "chocolate", "silver", "navy"]
-        img = plt.imread("doc/map.png")
-        fix, ax = plt.subplots()
-        ax.imshow(img, extent=[3.1, 7.5, 50.6, 53.7])
-        counter = 0
-        legendnumber = []
-    
-        # Retrieve coordinates of stations in the trajects
-        for value in self.traject.items():
-            x_coor = []
-            y_coor = []
-            
-            for stations in value[1][0]:
-                x_coor.append(self.stations[stations][0])
-                y_coor.append(self.stations[stations][1])
+    colors = ["green", "red", "aqua", "orange", "yellow", "lawngreen", "deepskyblue", "violet", "pink", "deeppink", "darkviolet", "grey", "salmon", "gold", "mediumseagreen", "mediumturquoise", "darkkhaki", "chocolate", "silver", "navy"]
+    img = plt.imread("doc/map.png")
+    fix, ax = plt.subplots()
+    ax.imshow(img, extent=[3.1, 7.5, 50.6, 53.7])
+    counter = 0
+    legendnumber = []
 
-            ax.plot(x_coor, y_coor, color=colors[counter], linestyle='dashed', marker='o', markersize=3)
-            counter += 1
-            legendnumber.append(counter) 
-
-        plt.title('Lijnvoering Noord- en Zuid-Holland')
-        ax.legend(legendnumber, loc="best")
-        plt.show()
-
-    def export(self, file):
-        """ Export besttraject and bestquality to a csv-file """
+    # Retrieve coordinates of stations in the trajects
+    for value in traject.items():
+        x_coor = []
+        y_coor = []
         
-        date_now = datetime.now()
-        date = date_now.strftime("%Y-%m-%d %H:%M:%S")
+        for stations in value[1][0]:
+            x_coor.append(all_stations[stations][0])
+            y_coor.append(all_stations[stations][1])
 
-        with open(file, "a", newline="") as csv_write:
-            writer = csv.writer(csv_write)
-            writer.writerow([date])
+        ax.plot(x_coor, y_coor, color=colors[counter], linestyle='dashed', marker='o', markersize=3)
+        counter += 1
+        legendnumber.append(counter) 
 
-            for key, value in self.traject.items():
-                writer.writerow([key, value])
-                
-            writer.writerow([self.quality])
-            writer.writerow([])
+    plt.title('Train lining system')
+    ax.legend(legendnumber, loc="best")
+    plt.show()
+
+def export(file, traject, quality):
+    """ 
+        Export the traject with the highest quality to a csv-file associated with the heuristic performed.  
+    """
+    
+    date_now = datetime.now()
+    date = date_now.strftime("%Y-%m-%d %H:%M:%S")
+
+    with open(file, "a", newline="") as csv_write:
+        writer = csv.writer(csv_write)
+        writer.writerow([date])
+
+        for key, value in traject.items():
+            writer.writerow([key, value])
+            
+        writer.writerow([quality])
+        writer.writerow([])
